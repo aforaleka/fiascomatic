fe1d22e890cc09fa940d344315faafaa89ccd777
Christopher Li <Christopherli2017@u.northwestern.edu>
2016-04-18 19:13:41 -0500

    updates

--- college.pl
+++ college.pl
@@ -15,8 +15,43 @@
 
 
 % Locations
+location(school_auditorium).
+location(shady_apartment_building).
+location(steachers_office).
+role_location(student,rooftop_Bar).
+role_location(student,empty_Library).
+location(football_field).
+location(sketchy_park).
+location(chemistry_lab).
+role_location(student,backroom_of_a_Starbucks).
+location(gym_bathroom).
+role_location(student,fraternity_Quad).
+role_location(ta,fraternity_Quad).
+role_location(student,inside_mom’s_car).
+role_location(ta,inside_mom’s_car).
+location(toilets_stall_with_no_toilet_paper).
+location(unknown_person’s_bedroom).
+location(burger_King).
+
 
 
 
 
 % Objects
+role_object(student, report_card_with_straight_Fs).
+object(handles_of_vodka).
+object(bag_of_dank_weed).
+object(stolen_macbook).
+role_object(student, tickets_to_Chance_the_Rapper).
+object(midterm_answers).
+object(brownie).
+role_object(student,fake_ID).
+object(mason_jar_of_fingernails).
+object(childhood_stuffed_animal).
+object(blonde_Wig).
+object(selfie_stick).
+role_object(student,blackmail_on_professor).
+role_object(ta,blackmail_on_professor).
+role_object(ta,blackmail_on_student).
+role_object(professor,blackmail_on_student).
+object(james_Bond_Pencil).
